00697adc8da9b3960e83ece9c5b28154902b430b
Joshua <satwa.fr@gmail.com>
2019-12-26 21:58:04 +0100

    Adding Firebase admin

--- live/index.js
+++ live/index.js
@@ -1,17 +1,33 @@
-var Handlers = require('./handlers')
+const Handlers = require('./handlers')
 
 exports.register = function (server, options, next) {
+    let io = require('socket.io')(server.listener)
 
-    var io = require('socket.io')(server.listener);
+    const firebaseAdmin = options.firebaseAdmin
 
-    io.on('connection', function (socket) {
+    io
+        .use(async (socket, next) => {
+            if(socket.handshake.query && socket.handshake.query.token){
+                try{
+                    const user = await firebaseAdmin.auth().verifyIdToken(socket.handshake.query.token)
+                    socket.decoded = user
+                    next()
+                }catch(err){
+                    console.log("LIVE API: Unauthorized user tried to access the API")
+                    next(new Error("Authentication error"))
+                }
+            }else{
+                console.log("LIVE API: Unauthorized user tried to access the API")
+                next(new Error("Authentication error"))
+            }
+        })
+        .on('connection', function (socket) {
+            console.log('New connection!')
 
-        console.log('New connection!')
-
-        socket.on('hello', Handlers.hello)
-        socket.on('newMessage', Handlers.newMessage)
-        socket.on('goodbye', Handlers.goodbye)
-    })
+            socket.on('hello', Handlers.hello)
+            socket.on('newMessage', Handlers.newMessage)
+            socket.on('goodbye', Handlers.goodbye)
+        })
 
     next()
 }
